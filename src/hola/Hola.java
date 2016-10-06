b0382166b8be8d6e5e1643c1f9236fb6fda8477e
Lizeth-Ortega-Vargas <1715110698@utec-tgo.edu.mx>
2016-10-06 12:20:41 -0500

    primera cometer

---
+++ src/hola/Hola.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola;
+
+/**
+ *
+ * @author Liz
+ */
+public class Hola {
+
+    public class Hola extends Matz
+    public static void main(String[] args) {
+{
+
+
+Hola objeto = new Hola();
+
+objeto.a=5;
+objeto.b=7;
+objeto.sumar();
+
+}//main
+}//class
+
+    }
+    
+}
